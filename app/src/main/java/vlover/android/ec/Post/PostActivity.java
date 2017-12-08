48e9150d72a6c5006be32bb6b658d4b957aee519
Brandon Macay <brandomacay@gmail.com>
2017-12-07 23:57:44 -0500

    xml Post part 1 by BMMR

---
+++ app/src/main/java/vlover/android/ec/Post/PostActivity.java
@@ -0,0 +1,16 @@
+package vlover.android.ec.Post;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vlover.android.ec.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+
+}

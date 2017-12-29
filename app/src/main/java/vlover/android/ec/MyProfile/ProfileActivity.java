8047ab2cba34a0465c0d22d0c35bac480d787059
Brandon Macay <brandomacay@gmail.com>
2017-12-29 01:39:48 -0500

    Vlover Brandon

---
+++ app/src/main/java/vlover/android/ec/MyProfile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package vlover.android.ec.MyProfile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vlover.android.ec.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
